44ff9a8bcee8a9a8d45445f1422baea799d7b564
Kathy Lu <kaffy.lu@gmail.com>
2015-09-15 16:09:59 -0400

    can get saved and requested posts for a user

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -11,3 +11,37 @@ router.get('/', function(req, res, next)
     res.send(posts);
   });
 });
+
+router.use('/:userId', function(req, res, next)
+{
+  User.findById(req.params.userId)
+  .then(function(user)
+  {
+    req.foundUser = user;
+    next();
+  })
+  .then(null, next);
+});
+
+router.get('/:userId', function(req, res, next)
+{
+  res.send(req.foundUser);
+});
+
+router.get('/:userId/saved', function(req, res, next)
+{
+  Postings.find().where({artistsWhoSaved: req.foundUser._id})
+  .then(function(postings)
+  {
+    res.send(postings);
+  });
+});
+
+router.get('/:userId/requested', function(req, res, next)
+{
+  Postings.find().where({artistsWhoRequested: req.foundUser._id})
+  .then(function(postings)
+  {
+    res.send(postings);
+  });
+});
