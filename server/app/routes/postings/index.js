82f8f7288e829a0604b12aacdbac31ae46d9ed03
Kathy Lu <kaffy.lu@gmail.com>
2015-09-15 01:57:21 -0400

    unloggedin user can add jobs to cart

--- server/app/routes/postings/index.js
+++ server/app/routes/postings/index.js
@@ -5,6 +5,7 @@ var Posting = mongoose.model('Posting');
 
 router.get('/', function(req, res, next)
 {
+  console.log("getting all postings for user", req.user)
   Posting.find()
   .then(function(postings)
   {
@@ -26,5 +27,24 @@ router.use('/:postingId', function(req, res, next)
 
 router.get('/:postingId', function(req, res, next)
 {
+  res.send(req.posting);
+});
+
+router.post('/:postingId', function(req, res, next)
+{
+  console.log("req.session.cart", req.session.cart)
+  //add to cart
+  if(req.session.cart)
+  {
+    console.log("index",req.session.cart.indexOf(req.posting._id), "req.posting._id", req.posting._id);
+    if(req.session.cart.indexOf(req.posting._id.toString())<0)
+    {
+      req.session.cart.push(req.posting._id.toString());
+    }
+  }
+  else
+  {
+    req.session.cart = [req.posting._id.toString()];
+  }
   res.send(req.posting);
 });
