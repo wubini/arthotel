14a16edff22aa93d79037028eb90c928421e6a06
Kathy Lu <kaffy.lu@gmail.com>
2015-09-15 02:04:54 -0400

    cleaned up posting model

--- server/db/models/posting.js
+++ server/db/models/posting.js
@@ -12,35 +12,5 @@ var schema = new mongoose.Schema({
   status: {type: String, default: "private", match: /(private|public)/},
   date: { type: Date, default: Date.now }
 });
-// generateSalt, encryptPassword and the pre 'save' and 'correctPassword' operations
-// are all used for local authentication security.
-var generateSalt = function () {
-    return crypto.randomBytes(16).toString('base64');
-};
-
-var encryptPassword = function (plainText, salt) {
-    var hash = crypto.createHash('sha1');
-    hash.update(plainText);
-    hash.update(salt);
-    return hash.digest('hex');
-};
-
-schema.pre('save', function (next) {
-
-    if (this.isModified('password')) {
-        this.salt = this.constructor.generateSalt();
-        this.password = this.constructor.encryptPassword(this.password, this.salt);
-    }
-
-    next();
-
-});
-
-schema.statics.generateSalt = generateSalt;
-schema.statics.encryptPassword = encryptPassword;
-
-schema.method('correctPassword', function (candidatePassword) {
-    return encryptPassword(candidatePassword, this.salt) === this.password;
-});
 
 mongoose.model('Posting', schema);
