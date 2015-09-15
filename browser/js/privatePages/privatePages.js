c53d3c5b9719c6ebb5afaf3de1d05f647d3a1994
Beckylee <Beckylee.Dell@tufts.edu>
2015-09-15 14:44:40 -0400

    got rid of dummy data

--- browser/js/privatePages/privatePages.js
+++ browser/js/privatePages/privatePages.js
@@ -13,12 +13,11 @@ app.config(function ($stateProvider) {
 
 
 app.controller('privatePageCtrl', function ($scope, AuthService, $state, allPostings, Session) {
-    console.log(allPostings);
 
+    //this will be dynamically changed
     $scope.client = true;
     $scope.activeJobs = [];
 
-    //will need to change 55f81ca3cf14af441f22fd69 to Session.id
     if($scope.client){
         allPostings.forEach(function(post){
             if(post.client == Session.id && status == 'started'){
@@ -31,31 +30,5 @@ app.controller('privatePageCtrl', function ($scope, AuthService, $state, allPost
                 $scope.activeJobs.push(post);
             }
         });
-    }
-    // $scope.activeJobs = [{
-    //     name: 'Project1',
-    //     photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPgXI7ScOx4mJylaE_DHY_T7Xs9VYuelvlADNELc3V6f2ssByA7QLBb08',
-    //     endDate: 'September 17, 2015',
-    //     _id: '55f81ca3cf14af441f22fd6b',
-    //     artist: {
-    //         name: 'Artist',
-    //         _id: 1234254
-    //     },
-    //     client: {
-    //         name: 'client',
-    //         _id: 8897234098
-    //     }
-    // },{
-    //     name: 'Project2',
-    //     photo: 'https://my.vetmatrixbase.com/clients/12679/images/Gorgeous_puppies.jpg',
-    //     artist: {
-    //         name: 'Artist',
-    //         _id: 123424245
-    //     },
-    //     client: {
-    //         name: 'client',
-    //         _id: 1234254
-    //     }
-
-    // }];
+    };
 });
