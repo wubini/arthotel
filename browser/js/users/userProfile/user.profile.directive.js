72a1bb1e1cc9d73e1c758c821f73dc428e3a719b
Kathy Lu <kaffy.lu@gmail.com>
2015-09-16 21:35:04 -0400

    cancel editing works

--- browser/js/users/userProfile/user.profile.directive.js
+++ browser/js/users/userProfile/user.profile.directive.js
@@ -7,13 +7,26 @@ app.directive("userProfile", function()
     },
     controller: function($scope)
     {
+      $scope.userCopy = {};
+      for(var key in $scope.user)
+      {
+        $scope.userCopy[key]= $scope.user[key];
+      }
       console.log("user logged in",$scope.user);
       $scope.editing = false;
       $scope.toggleEditing = function()
       {
         $scope.editing = !$scope.editing;
-      };
+      }
+
+      $scope.restoreValuesToSaved = function()
+      {
+        for(var key in $scope.user)
+        {
+          $scope.user[key]= $scope.userCopy[key];
+        }
+      }
     },
     templateUrl: 'js/users/userProfile/user.profile.html'
-  };
-});
+  }
+})
