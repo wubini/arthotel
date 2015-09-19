e44fb29b9777538b3d62dbb3ff3a625149241759
Beckylee <dr.oreo@gmail.com>
2015-09-19 13:17:15 -0400

    cannot add a new post with the same title as an active posting

--- browser/js/postings/newPosting/newPosting.js
+++ browser/js/postings/newPosting/newPosting.js
@@ -23,13 +23,20 @@ app.controller('newPostingCtrl', function ($scope, currentUser, AuthService, $st
       photos: []
     };
 
+    $scope.titleIssue = false;
 
     $scope.sendPost = function(postInfo){
       if(!currentUser) return;
 
+      $scope.titleIssue = false;
+
       PostingFactory.createNewPosting(postInfo)
       .then(function(newPost){
-        $state.go('detailedPosting', {postingId:newPost._id});
+        if(newPost._id){
+          $state.go('detailedPosting', {postingId:newPost._id});
+        }else{
+          $scope.titleIssue = true;
+        }
       });
 
     };
