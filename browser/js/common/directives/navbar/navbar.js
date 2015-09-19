2d23d8031b0baa94ccddea5f1e2efa3214e82cfa
Kathy Lu <kaffy.lu@gmail.com>
2015-09-19 16:51:51 -0400

    search by tag works, but need to fix tags in creating-post!

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -5,6 +5,7 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
         scope: {},
         templateUrl: 'js/common/directives/navbar/navbar.html',
         link: function (scope) {
+
             // $('li').invertImgText();
             // document.querySelector('.navbar > .container').invertImgText();
             scope.items = [
--- browser/js/postings/allPostings/allPostings.js
+++ browser/js/postings/allPostings/allPostings.js
@@ -1,13 +1,13 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('allPostings', {
-        url: '/postings',
+        url: '/postings?search',
         templateUrl: 'js/postings/allPostings/allPostings.html',
         controller: 'allPostingsCtrl',
         resolve: {
           currentUser: function(AuthService){
             return AuthService.getLoggedInUser();
           },
-          activePosts: function(PostingFactory){
+          activePostings: function(PostingFactory){
             return PostingFactory.getAllPostings()
             .then(function(postings){
               return postings.filter(function(post){
@@ -20,9 +20,30 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('allPostingsCtrl', function ($scope, activePosts, AuthService, $state, PostingFactory, currentUser) {
+app.controller('allPostingsCtrl', function ($scope, activePostings, AuthService, $state, $stateParams, PostingFactory, currentUser) {
 
-  $scope.postings = activePosts;
+  $scope.searchTerms = {};
+  $scope.postings = activePostings;
+
+  if($stateParams.search)
+  {
+    $scope.searchTerms = $stateParams.search.split(" ");
+    $scope.postings = activePostings.filter(function(posting)
+    {
+      var contains = true;
+      $scope.searchTerms.forEach(function(term)
+      {
+        console.log(`checking ${posting.title} for term ${term}`);
+        if(posting.tags.indexOf(term)===-1)
+        {
+          console.log("not found in tags", posting.tags);
+          contains = false;
+        }
+      })
+
+      return contains;
+    })
+  }
 
   $scope.newPost = function(){
     $state.go('newPosting');
