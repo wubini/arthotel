d3b27b8edf6b35d0bd8b80db6ca4c083eb2288e1
Beckylee <dr.oreo@gmail.com>
2015-09-21 13:21:11 -0400

    gave active jobs directive isolate scopes

--- browser/js/privatePages/directives/activeJobs/client.active.job.js
+++ browser/js/privatePages/directives/activeJobs/client.active.job.js
@@ -2,6 +2,9 @@ app.directive('clientActiveJob', function ($state, PostingFactory) {
     return {
         restrict: 'E',
         templateUrl: 'js/privatePages/directives/activeJobs/client.active.job.html',
+        scope:{
+          job: "="
+        },
         link: function(scope){
           // elem.on('click', function(){
           //   $state.go('detailedPosting', {postingId: scope.job._id});
