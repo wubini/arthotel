b1f9468a30b7872a1b5fd39a4fe64b798dc29720
Kathy Lu <kaffy.lu@gmail.com>
2015-09-21 00:34:51 -0400

    some changes?

--- server/app/configure/authentication/google.js
+++ server/app/configure/authentication/google.js
@@ -15,8 +15,6 @@ module.exports = function (app) {
     };
 
     var verifyCallback = function (accessToken, refreshToken, profile, done) {
-
-      console.log("profile", profile);
         UserModel.findOne({ 'google.id': profile.id }).exec()
             .then(function (user) {
 
