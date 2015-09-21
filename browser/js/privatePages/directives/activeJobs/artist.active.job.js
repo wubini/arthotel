4aee66f45b775e531ba44821f70fc39115646468
Kathy Lu <kaffy.lu@gmail.com>
2015-09-21 02:06:26 -0400

    current project info showing again

--- browser/js/privatePages/directives/activeJobs/artist.active.job.js
+++ browser/js/privatePages/directives/activeJobs/artist.active.job.js
@@ -1,26 +1,18 @@
-app.directive('job', function ($state, PostingFactory) {
+app.directive('artistActiveJob', function ($state, PostingFactory) {
     return {
         restrict: 'E',
-        templateUrl: 'js/privatePages/directives/activeJobs/job.html',
+        templateUrl: 'js/privatePages/directives/activeJobs/artist.active.job.html',
         link: function(scope){
           // elem.on('click', function(){
           //   $state.go('detailedPosting', {postingId: scope.job._id});
           // });
 
           scope.artistCompleted = function(){
-            
+
             PostingFactory.changePostingStatus(scope.job._id, "pendingApproval")
             .then(function(posting)
             {
               $state.go('privatePage',{tab: scope.tab}, {reload:true});
-            });
-          }
-
-          scope.clientApproved = function(){
-            PostingFactory.changePostingStatus(scope.job._id, "complete")
-            .then(function(posting)
-            {
-              $state.go('privatePage',{tab: scope.tab}, {reload: true});
             });
           }
         }
