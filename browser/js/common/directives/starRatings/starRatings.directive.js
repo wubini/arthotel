9aa872af9b1750b2eced5bf64d94213fbcd850a9
Beckylee <dr.oreo@gmail.com>
2015-09-21 14:09:30 -0400

    gave stars an isolate scope

--- browser/js/common/directives/starRatings/starRatings.directive.js
+++ browser/js/common/directives/starRatings/starRatings.directive.js
@@ -2,6 +2,9 @@ app.directive('starRatings', function () {
     return {
         restrict: 'E',
         templateUrl: 'js/common/directives/starRatings/starratings.html',
+        scope:{
+          rating:"="
+        }
         link: function(scope){
 
           scope.stars = [1, 2, 3, 4, 5];
