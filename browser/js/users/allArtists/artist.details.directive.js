fc952088c19c5251de95a09c3907dbb086963698
Beckylee <dr.oreo@gmail.com>
2015-09-20 21:04:14 -0400

    admin can view all users

--- browser/js/users/allArtists/artist.details.directive.js
+++ browser/js/users/allArtists/artist.details.directive.js
@@ -3,8 +3,12 @@ app.directive("artistDetails", function()
   return {
     restrict: 'EA',
     scope: {
-      artist:"="
+      artist:"=",
+      currentUser:"="
     },
-    templateUrl: 'js/users/allArtists/artist.details.html'
+    templateUrl: 'js/users/allArtists/artist.details.html',
+    link: function(scope){
+      console.log(scope);
+    }
   }
 });
