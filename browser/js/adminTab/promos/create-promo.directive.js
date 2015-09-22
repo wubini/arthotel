70625feb187fbe0d945bda408ee195015a584793
Austin <tashoecraft@gmail.com>
2015-09-21 21:52:56 -0400

    saving

--- browser/js/adminTab/promos/create-promo.directive.js
+++ browser/js/adminTab/promos/create-promo.directive.js
@@ -1,7 +1,7 @@
-app.directive('createPromo' , PromoFactory => {
+app.directive('createPromo', PromoFactory => {
   return {
     restrict: 'E',
-    templateUrl: 'create.promo.template.html',
+    templateUrl: 'js/adminTab/promos/create-promo-template.html',
     link: scope => {
       scope.createPromo = newPromo => {
         PromoFactory.createPromo(newPromo);
