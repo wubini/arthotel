01254002c610ed7625e7159263a9cd46de92f714
Beckylee <dr.oreo@gmail.com>
2015-09-22 15:14:58 -0400

    logged out users prompted to login on request

--- browser/js/postings/detailedPosting/saveRequest.directive.js
+++ browser/js/postings/detailedPosting/saveRequest.directive.js
@@ -1,7 +1,13 @@
 app.directive('saveRequest', function () {
     return {
         restrict: 'E',
-        templateUrl: 'js/postings/detailedPosting/saverequest.html'
+        templateUrl: 'js/postings/detailedPosting/saverequest.html',
+        link: function(scope){
+          scope.toolTip = "";
+          if(!scope.currentUser){
+            scope.toolTip = "You must be logged in to request a project!";
+          }
+        }
 
     };
 });
